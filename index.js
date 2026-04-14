6a1a6e99dd023cc3b5af850a35c57b6d2a8d4c82
Mohammad Kaif <mohdkaif90275@gmail.com>
2026-04-14 15:11:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  { id: 1, name: "Kaif", age: 22 },
+  { id: 2, name: "Ali", age: 23 },
+  { id: 3, name: "Rahul", age: 21 },
+  { id: 4, name: "Aman", age: 24 },
+  { id: 5, name: "Zaid", age: 22 },
+  { id: 6, name: "Rohit", age: 25 },
+  { id: 7, name: "Imran", age: 23 },
+  { id: 8, name: "Arjun", age: 22 },
+  { id: 9, name: "Vikram", age: 26 },
+  { id: 10, name: "Sameer", age: 24 }
+];
+
+// GET all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// POST new user
+app.post("/users", (req, res) => {
+  const newUser = { id: users.length + 1, ...req.body };
+  users.push(newUser);
+  res.json(newUser);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
